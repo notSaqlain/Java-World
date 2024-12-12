7f14ff688a82512a40c60c1549628aebcb9a6877
notSaqlain <iamnotsaqlain@gmail.com>
2024-12-12 09:14:28 +0100

    Battt

--- Country.java
+++ Country.java
@@ -7,11 +7,18 @@ public class Country {
     private ArrayList<Paese> elenco;
 
     private void LoadFile(String FileCSV) {
-        File dati = new File("./data/paesi.csv");
+        File dati = new File(FileCSV);
         try {
             Scanner sc = new Scanner(dati);
             while (sc.hasNextLine()) {
                 String riga = sc.nextLine();
+                String fld[] = riga.split(",");
+                if (fld.length != 3) {
+                    System.out.println("Errore: riga non conforme");
+                    continue;
+                }
+                Paese p = new Paese(fld[0], fld[1], fld[2]);
+                elenco.add(p);
             }
             sc.close();
         } catch (FileNotFoundException e) {
