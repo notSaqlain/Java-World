2cf135065629d0ae84e1e83c086a64be84035652
notSaqlain <iamnotsaqlain@gmail.com>
2024-12-12 09:04:04 +0100

    Add LoadFile method to Country class for loading country data from CSV

--- Country.java
+++ Country.java
@@ -1,7 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Country {
     private ArrayList<Paese> elenco;
+
+    private void LoadFile(String FileCSV) {
+        File dati = new File("./data/paesi.csv");
+        try {
+            Scanner sc = new Scanner(dati);
+            while (sc.hasNextLine()) {
+                String riga = sc.nextLine();
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
     
     public Country(String FileCSV) {
         System.out.println("Country constructor");
