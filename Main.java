12cdf0ac6384a62fc3508577c9eaee1ddfd0f72f
notSaqlain <iamnotsaqlain@gmail.com>
2024-12-12 08:39:12 +0100

    Add file handling and initialize ArrayList in main method

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
+import java.io.File;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        File dati = new File("./data/paesi.csv");
+        ArrayList<> paesi = new ArrayList<>();
         
     }
 }
