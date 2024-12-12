9d811699916b5029d2b41c277f792ebef025fb9e
notSaqlain <iamnotsaqlain@gmail.com>
2024-12-12 08:49:30 +0100

    Add Paese class with attributes and getter/setter methods

--- paese.java
+++ paese.java
@@ -1,3 +1,35 @@
 public class Paese {
-    
+    String nome;
+    String continente;
+    String popolazione;
+
+    public Paese(String nome, String continente, String popolazione) {
+        this.nome = nome;
+        this.continente = continente;
+        this.popolazione = popolazione;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getContinente() {
+        return continente;
+    }
+
+    public String getPopolazione() {
+        return popolazione;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setContinente(String continente) {
+        this.continente = continente;
+    }
+
+    public void setPopolazione(String popolazione) {
+        this.popolazione = popolazione;
+    }
 }
